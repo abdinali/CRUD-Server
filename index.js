54c1d29fef1a9970080b6f6d88d86eef0bf861e5
Abdinasir Ali <131626624+abdinasir5@users.noreply.github.com>
2024-05-08 23:48:51 -0400

    Added form page for new products

--- index.js
+++ index.js
@@ -16,15 +16,28 @@ async function main() {
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({}) // find all products
+    const products = await Product.find({}); // find all products
     res.render('products/index', { products });
+})
+
+// Form For New Products
+app.get('/products/new', (req, res) => {
+    res.render('products/new'); // render form
+})
+
+// Form Submission
+app.post('/products', async (req, res) => {
+    const newProduct = await new Product(req.body);
+    newProduct.save();
+    res.redirect(`/products/${newProduct._id}`); // redirect to details page
 })
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
-    const selectedProduct = await Product.findById(id);
+    const selectedProduct = await Product.findById(id); // find specific product from id
     res.render('products/show', { selectedProduct });
 })
 
